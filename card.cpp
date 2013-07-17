c04eb72cbef5033e16f067dbd7b9113850531e85
Matt Kirwan <matthewkirwan@gmail.com>
2013-07-17 22:18:29 +0100

    Created some new cards and started the ball rolling on passing through the value. Tight like a tiger.

--- card.cpp
+++ card.cpp
@@ -7,57 +7,29 @@ Card::Card()
 
 }
 
-std::string Card::getCard(std::string card_identifier)
+std::string Card::Top(std::string card_number)
 {
-    return card_identifier;
+    return std::string(" _________\n/         \\\n|"+card_number+"        |\n");
 }
 
-std::string Card::D1()
+std::string Card::Bottom(std::string card_number)
 {
-    return std::string(" ___\n|A  |\n| /\\|\n|_\\/|\n");
+    return std::string("|        "+card_number+"|\n\\_________/\n\n");
 }
 
-std::string Card::D2()
+std::string Card::Diamond()
 {
-    return std::string(" ___\n|2  |\n| /\\|\n|_\\/|\n");
+    return std::string("|         |\n|   /\\    |\n|  {  }   |\n|   \\/    |\n|         |\n");
 }
-
-std::string Card::D3()
+std::string Card::Heart()
 {
-    return std::string(" ___\n|3  |\n| /\\|\n|_\\/|\n");
+    return std::string("|   _ _   |\n|  / ^ \\  |\n|  \\   /  |\n|   \\ /   |\n|    `    |\n");
 }
-
-std::string Card::D4()
+std::string Card::Spade()
 {
-    return std::string(" ___\n|4  |\n| /\\|\n|_\\/|\n");
+    return std::string("|    ,    |\n|   / \\   |\n|  (_ _)  |\n|   /_\\   |\n|         |\n");
 }
-
-std::string Card::D5()
+std::string Card::Club()
 {
-    return std::string(" ___\n|5  |\n| /\\|\n|_\\/|\n");
-}
-
-std::string Card::D6()
-{
-    return std::string(" ___\n|6  |\n| /\\|\n|_\\/|\n");
-}
-
-std::string Card::D7()
-{
-    return std::string(" ___\n|7  |\n| /\\|\n|_\\/|\n");
-}
-
-std::string Card::D8()
-{
-    return std::string(" ___\n|8  |\n| /\\|\n|_\\/|\n");
-}
-
-std::string Card::D9()
-{
-    return std::string(" ___\n|9  |\n| /\\|\n|_\\/|\n");
-}
-
-std::string Card::D10()
-{
-    return std::string(" ___\n|10 |\n| /\\|\n|_\\/|\n");
+    return std::string("|    _    |\n|   (_)   |\n|  (_)_)  |\n|   /_\\   |\n|         |\n");
 }
--- card.h
+++ card.h
@@ -6,17 +6,14 @@ class Card
 {
     public:
         Card();
-        std::string getCard(std::string card_identifier);
-        std::string D1();
-        std::string D2();
-        std::string D3();
-        std::string D4();
-        std::string D5();
-        std::string D6();
-        std::string D7();
-        std::string D8();
-        std::string D9();
-        std::string D10();
+        
+        std::string Top(std::string card_number);
+        std::string Bottom(std::string card_number);
+
+        std::string Diamond();
+        std::string Heart();
+        std::string Spade();
+        std::string Club();
 };
 
 #endif
