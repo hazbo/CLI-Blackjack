c851a92d4ad36ef031d39c72a061314ff600c99f
Harry Lawrence <harry.lawrence@groundsix.com>
2013-07-17 12:58:34 +0100

    Outputting some cards

--- card.h
+++ card.h
@@ -10,6 +10,13 @@ class Card
 		std::string D1();
 		std::string D2();
 		std::string D3();
+		std::string D4();
+		std::string D5();
+		std::string D6();
+		std::string D7();
+		std::string D8();
+		std::string D9();
+		std::string D10();
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -6,5 +6,14 @@
 int main()
 {
 	Blackjack Game;
+  Card newCard;
+
+  // For testing, we will output the Ace, 5, 6 and 10 of diamonds
+  // Just for the crack
+  std::cout << newCard.D5();
+  std::cout << newCard.D6();
+  std::cout << newCard.D1();
+  std::cout << newCard.D10();
+
 	return 0;
 }
